4cf4943bb3d6131dda2a102e863d7e4ea77fdab9
Aimilios <aimilios.ioannou@cern.ch>
2019-03-06 11:54:39 +0100

    producing lhe file only

---
+++ AToZhToNuNuBB/M225/Produce_LHE_file_only.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("LHE")
+
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.EventContent.EventContent_cff')
+
+process.source = cms.Source("EmptySource"
+                            )
+
+process.load("GeneratorInterface.LHEInterface.ExternalLHEAsciiDumper_cfi")
+process.externalLHEAsciiDumper.lheFileName = cms.string('output.lhe')
+#process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = 21
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100000) )
+
+process.load("Configuration.GenProduction.LHE_Producer_AToZhToNuNuBB_M225_cff")
+
+process.externalLHEProducer.nEvents = process.maxEvents.input.value()
+process.externalLHEProducer.outputFile = cms.string('cmsgrid_final.lhe')
+
+process.externalLHEProducer.args = cms.vstring('../AToZhToNuNuBB_01j_4f_M225_tarball.tar.xz')
+
+process.out = cms.OutputModule("PoolOutputModule",
+                               splitLevel = cms.untracked.int32(0),
+                               eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+                               outputCommands = process.LHEEventContent.outputCommands,
+                               fileName = cms.untracked.string('lheoutputfile.root'),
+                               dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('LHE')
+        )
+                               )
+
+process.lhe_step = cms.Path(process.externalLHEProducer)
+process.endjob_step = cms.EndPath(process.externalLHEAsciiDumper)
+process.LHE_output = cms.EndPath(process.out)
+
+#process.schedule = cms.Schedule(process.lhe_step,process.endjob_step)
+
